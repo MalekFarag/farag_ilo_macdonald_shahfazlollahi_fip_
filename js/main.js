728569b6154d0c5ea9a97dd62f25704fcdbcd021
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-01-30 16:09:52 -0500

    mobile build complate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log('js linked.')
+console.log('js linked.');
+
+
+const nav = document.querySelector('.mainNav'),
+      header = document.querySelector('.header'),
+      burger = document.querySelector('.burger');
+
+        function navToggle(){
+            console.log('nav toggled.')
+            nav.classList.toggle('navOn');
+        }
+
+      burger.addEventListener('click', navToggle);
