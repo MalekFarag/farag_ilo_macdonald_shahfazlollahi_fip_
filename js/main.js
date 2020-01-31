597eedac125c94ca10c9e86c49833dcd53962016
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-01-30 21:46:35 -0500

    adding additional copy

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ console.log('js linked.');
 
 const nav = document.querySelector('.mainNav'),
       header = document.querySelector('.header'),
-      burger = document.querySelector('.burger');
+      burger = document.querySelector('.burger'),
+      navItems = document.querySelectorAll('.navItem');
 
         function navToggle(){
             console.log('nav toggled.')
@@ -11,3 +12,8 @@ const nav = document.querySelector('.mainNav'),
         }
 
       burger.addEventListener('click', navToggle);
+      navItems.forEach(item =>{
+        item.addEventListener('click', function(){
+          nav.classList.remove('navOn');
+        })
+      })
