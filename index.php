2eb718418a6bed6c1c85dde6c5f11f45f8fc47ca
junior231 <43250479+junior231@users.noreply.github.com>
2020-04-08 01:32:00 -0400

    mobile site 90%

--- index.php
+++ index.php
@@ -76,54 +76,40 @@
     </header>
 
     <!-- main content container -->
-    <main id="content">
+    <main id="content" class="container">
 
         <!-- about section -->
         <section class="hero">
-            <img src="images/rose.png" class='rose' alt="rose">
-            <div class="logoCircle">
-                <p>SAFE</p>
-                <div class="line"></div>
-                <p>SPACE</p>
-            </div>
+            <img src="images/hero_image.jpg" class="hero-image" alt="Image of Two Women">
 
             <div class="about">
-                <h2>About</h2>
-                <p class='copy'>We are a safe space to discuss and provide information regarding safe sex for everyone! We provide information especially for the LGBTQA community, explaining how to keep them healthy and protect themselves; no matter a persons sexual preference or gender identity.</p>
+                <h3>LET'S TALK ABOUT SEX</h3>
+                <p class='copy'>We are a safe space to discuss and provide information regarding safe sex for everyone! We provide information especially for the LGBTQA community, explaining how to keep them healthy and
+                protect themselves; no matter a persons sexual preference or gender identity..</p>
             </div>
         </section>
 
         <!-- about campaign section -->
         <section id="campaign">
-            <div class="buzzwords">
-                <div class="buzz">
-                    <i class="fas fa-hotdog"></i>
-                    <p>Safe sex is great sex <br>
-                        <span>info on safe sex</span></p>
-                </div>
-
-                <div class="buzz">
-                    <i class="fas fa-dove"></i>
-                    <p>The Birds and the Bees <br>
-                        <span>any gender relationship</span</p>
-                </div>
-
-                <div class="buzz">
-                    <i class="fas fa-book"></i>
-                    <p>Community Support <br>
-                        <span class="learn">Let's learn together</span></p>
-                </div>
-            </div>
 
             <div class="info">
-                <h2>Information</h2>
-                <p class='copy'>At an age where most are still struggling to come to terms with who they are, as a society, we have failed to do enough ease or make the journey less confusing. This why we have launched the campaign termed "Safe space: The birds and the Bees" where we provide information about safer sex in the LBGTQA community.</p>
+                <h3>INFORMATION</h3>
+                <p class='copy'>At an age where most are still struggling to come to terms with who they are, as a society, we have failed to do enough ease or make the journey less confusing. This why we have launched the campaign termed "Let's Talk About It", where we provide information about safer sex in the LBGTQA community.</p>
 
+                <div class="links">
+                <a href="talk.php" class="campaign-link">Get Talking</a>
+                <a href="support.php" class="campaign-link">Community Support</a>
+                </div>
             </div>
-            
 
-            <img src="images/rose.png" class='rose' alt="rose">
+            <div class="content-pic">
+            <img src="images/content_pic.jpg" class="content__img" alt="Image of Two Individuals">
+            </div>
+        
+
         </section>
+
+        
 
         <!-- loading 3 posts -->
         <div class="blog">
@@ -140,41 +126,21 @@
             <?php endwhile; ?>
         </div>
 
-
-        <!-- contact section -->
-        <section id="contact">
-            <img src="images/rose.png" class='rose' alt="">
-            <h2>We want to hear from you!</h2>
-            <p class='copy'>We're here to help and would love to answer any questions you have!
-            </p>
-            <?php echo !empty($message)? $message: ''; ?>
-            <form action="index.php" method="post">
-                <input type="text" name='name' placeholder="name" required>
-                <input type="email" name='email' placeholder="email" required>
-                <input type="text" name='phone' placeholder="phone" required>
-                <textarea name="body" id="body" placeholder="message" required></textarea>
-                <button name="submit">Send</button>
-            </form>
-
-           
-        </section>
-
         <!-- donate section -->
         <section id="donate">
-        <img src="images/rose.png" class='rose' alt="">
-            <h2>Help us spread the word</h2>
-            <p class='copy'>In partnership with the Regional HIV/AIDS Connection, you can support us; helping raise awareness and keep everyone healthy and protected</p>
+            <h2>Support The Cause</h2>
+            <p class='copy'>Help us spread the word, in partnership with              
+            the Regional HIV/AIDS Connection, you can support us; helping to spread the word and keep everyone healthy and protected</p>
             <!-- link to paypal or something -->
             <button type="submit">Donate</button>
         </section>
 
-    </main>
-    <!-- end of main content -->
-
-
-    <footer class="footer">
         <div class="newsletter">
-            <h4>Stay Updated with us!</h4>
+            <div class="news-image">
+            <img src="images/pride.jpg" class="news-pic" alt="image of a protester at a rally">
+            </div>
+            <h3>Stay Updated with us!</h3>
+            <p class="copy">Become a member of our ever-growing community by signing up. Read the latest blog posts and get updated on upcoming events and activities.</p>
             <?php echo !empty($message)? $message: ''; ?>
             <form action="index.php" method="post">
                 <input type="text" name='name2' placeholder="name" required>
@@ -183,15 +149,32 @@
                 <button name="signup">submit</button>
             </form>
         </div>
-        <nav class="footerNav">
-            <ul class="navList">
-                <a href="#content"><li class="navItem">Home</li></a>
-                <a href="#campaign"><li class="navItem">Campaign</li></a>
-                <a href="#about"><li class="navItem">About Us</li></a>
-                <a href="#contact"><li class="navItem">Contact</li></a>
-                <a href="#donate"><li class="navItem">Donate</li></a>
-            </ul>
-        </nav>
+
+         <!-- contact section -->
+         <section id="contact">
+            <h3>Contact Us</h3>
+
+            <div class="contact-deets">
+            <p class="copy">We are here to listen and would love to answer any questions you have!</p>
+            <p class='copy'>We want to help so feel free to ask us anything; no matter how uncomfy it might be!</p>
+            </div>
+
+            <?php echo !empty($message)? $message: ''; ?>
+            <div class="contact-form">
+            <form action="index.php" method="post">
+                <input type="text" name='name' placeholder="name" required>
+                <input type="email" name='email' placeholder="email" required>
+                <input type="text" name='phone' placeholder="phone" required>
+                <textarea name="body" id="body" placeholder="message" required></textarea>
+                <button name="submit">Send</button>
+            </form>
+            </div>
+
+           
+        </section>
+
+
+        <footer class="footer">
 
         <div class="social">
             <a href="#"><i class="fab fa-facebook-f"></i></a>
@@ -199,12 +182,16 @@
             <a href="#"><i class="fab fa-twitter"></i></a>
             <a href="#"><i class="fab fa-youtube"></i></a>
         </div>
-    </footer>
 
-    <div class="extra">
+        <div class="extra">
             <p>HIV/AIDS Connection&copy; - London 2020</p>
         <a href="admin/index.php">admin area</a>
         </div>
+    </footer>
+
+
+    </main>
+    <!-- end of main content --> 
 
 
     <script src="js/main.js"></script>
