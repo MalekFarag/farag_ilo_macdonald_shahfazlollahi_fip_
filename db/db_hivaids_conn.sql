447f80333b6d04ded3f1c631a1b8bc9cf00ef54a
Malek F <43182938+MalekFarag@users.noreply.github.com>
2020-04-11 18:25:36 -0400

    updating database

--- db/db_hivaids_conn.sql
+++ db/db_hivaids_conn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 06, 2020 at 10:53 PM
+-- Generation Time: Apr 11, 2020 at 10:25 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -62,7 +62,16 @@ CREATE TABLE IF NOT EXISTS `tbl_blog` (
   `text` text NOT NULL,
   `date` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_blog`
+--
+
+INSERT INTO `tbl_blog` (`id`, `title`, `sub_title`, `image`, `author`, `text`, `date`) VALUES
+(6, 'Why RHAC is totally awesome', 'Is it Really?', '9e5ab5f072f31ac70accf628e0358907.jpg', 'Malek Farag', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'July 21st 2019'),
+(7, 'RHAC and how they help People!', 'How they have hanged people\'s lives!', '27304e7abfa7cfe16372900c35397083.jpg', 'Malek Farag', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'Jan 27th 2020'),
+(8, 'RHAC Making a Difference', 'How they\'re impacting the community', 'f62b4120efe3def53681f6eda7fccc84.jpg', 'Malek Farag', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 'March 22nd 2020');
 
 -- --------------------------------------------------------
 
@@ -78,7 +87,14 @@ CREATE TABLE IF NOT EXISTS `tbl_customers` (
   `phone` varchar(16) NOT NULL,
   `ip` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_customers`
+--
+
+INSERT INTO `tbl_customers` (`id`, `name`, `email`, `phone`, `ip`) VALUES
+(3, 'Malek Farag', 'FaragMalek14@gmail.com', '000-000-0000', '::1');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
